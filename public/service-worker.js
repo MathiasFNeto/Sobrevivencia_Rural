9b1649d90e35f2f7f81a741e43312bcf247848bf
Mathias <mathiasfneto@gmail.com>
2026-05-18 20:41:27 -0300

    Melhora cache offline dos assets

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,28 +1,60 @@
-const CACHE_NAME = 'sobrevivencia-rural-v2';
+const CACHE_NAME = 'sobrevivencia-rural-v3';
 
 const urlsToCache = [
   './',
   './index.html',
   './style.css',
   './game.js',
+  './service-worker.js',
   './manifest.json',
   './assets/icons/icon-512.png',
   './assets/audio/dia.mp3',
   './assets/audio/noite.mp3',
-  './assets/audio/latido.mp3'
+  './assets/audio/latido.mp3',
+  './assets/sprites/house/house.png',
+  './assets/sprites/galinha/sprite_04.png',
+  './assets/sprites/galinha/sprite_05.png',
+  './assets/sprites/galinha/sprite_06.png',
+  './assets/sprites/galinha/sprite_07.png',
+  './assets/sprites/player/adventurer_idle.png',
+  './assets/sprites/player/adventurer_walk1.png',
+  './assets/sprites/player/adventurer_walk2.png',
+  './assets/sprites/player/adventurer_action1.png',
+  './assets/sprites/player/adventurer_action2.png',
+  './assets/sprites/player/adventurer_kick.png',
+  './assets/sprites/zombie/zombie_idle.png',
+  './assets/sprites/zombie/zombie_walk1.png',
+  './assets/sprites/zombie/zombie_walk2.png',
+  './assets/sprites/zombie/zombie_hurt.png',
+  './assets/sprites/zombie/zombie_action1.png',
+  './assets/sprites/zombie/zombie_action2.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
   );
 });
 
 self.addEventListener('fetch', event => {
+  if(event.request.method !== 'GET')return;
+
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => response || fetch(event.request).then(fetchResponse => {
+        const requestUrl = new URL(event.request.url);
+
+        if(requestUrl.origin === self.location.origin && fetchResponse.ok){
+          const responseClone = fetchResponse.clone();
+
+          caches.open(CACHE_NAME)
+            .then(cache => cache.put(event.request, responseClone));
+        }
+
+        return fetchResponse;
+      }))
   );
 });
 
